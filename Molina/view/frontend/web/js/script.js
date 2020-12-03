39bb54a6ffba15dba58cf99735b311544c666050
JoseAntonioMolina <jamr0025@gmail.com>
2020-12-03 19:36:13 +0100

    implementacion final modulo Molina close #1

--- Molina/view/frontend/web/js/script.js
+++ Molina/view/frontend/web/js/script.js
@@ -1,6 +1,8 @@
 define(['jquery'], function($) {
     return function(config, element) {
-
+        $(element).on('click','#buttonNote',function (){
+            alert("La nota mas alta es: "+config.notaAlta);
+        });
         $(element).on('click','#buttonMarks',function (){
             if( $(".notas").is(":visible")){
                 $(element).find(".notas").hide();
@@ -9,5 +11,6 @@ define(['jquery'], function($) {
             }
 
         });
+
     }
 });
